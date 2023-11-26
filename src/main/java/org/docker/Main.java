88ac5bbc5dbb0a21978012a26e0c9d6975c50bc7
Omnia <omniaeid266@gmail.com>
2023-11-25 21:23:04 -0600

    main class modfied

--- src/main/java/org/docker/Main.java
+++ src/main/java/org/docker/Main.java
@@ -2,13 +2,8 @@ package org.docker;
 
 public class Main {
     public static void main(String[] args) {
-<<<<<<< HEAD
 
         System.out.println("hello Ismail");
-
         System.out.println("any changes???");
-=======
-        System.out.println("Hello world!");
->>>>>>> 463c9c4 (docker jenkins files created)
     }
 }
